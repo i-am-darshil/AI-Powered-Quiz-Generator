612870b87b20ac583cb655e2f32e6ba37839210b
Darshil Shah <darshilshah9991@gmail.com>
2023-07-14 16:35:48 +0530

    Connect to supabase and add user to db when signed in

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,5 +1,16 @@
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
+import { createClient } from "@supabase/supabase-js";
+
+const supabaseUrl = process.env.SUPABASE_URL;
+const supabaseKey = process.env.SUPABASE_ANON_PUBLIC_KEY;
+const options = {
+  auth: {
+    persistSession: false,
+  },
+};
+
+let supabase = createClient(supabaseUrl, supabaseKey, options);
 
 const handler = NextAuth({
   providers: [
@@ -10,7 +21,13 @@ const handler = NextAuth({
   ],
   callbacks: {
     async session({ session }) {
+      const { data, error } = await supabase
+        .from("users")
+        .select()
+        .eq("email", session.user.email);
+      session.user.id = data[0].id.toString();
       console.log("Session callback session : ", JSON.stringify(session));
+
       return session;
     },
     async signIn({ profile }) {
@@ -19,9 +36,33 @@ const handler = NextAuth({
           "User Signed in callback, profile : ",
           JSON.stringify(profile)
         );
+
+        const { data, error } = await supabase
+          .from("users")
+          .select()
+          .eq("email", profile.email);
+
+        // if not, create a new document and save user in MongoDB
+        if (data.length == 0) {
+          const { error } = await supabase.from("users").insert({
+            email: profile.email,
+            username: profile.name,
+            image: profile.picture,
+          });
+
+          if (error) {
+            console.error(`Failed to add user ${profile.email} to database`);
+
+            return false;
+          }
+          console.log(`Added user ${profile.email} to database`);
+        } else {
+          console.log(`User ${profile.email} exists in our database`);
+        }
+
         return true;
       } catch (err) {
-        console.log(err);
+        console.error(err);
         return false;
       }
     },
